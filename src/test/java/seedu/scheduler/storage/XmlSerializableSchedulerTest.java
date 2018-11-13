54ebbed7cf0a5c2809449dce0c9032a941072610
Jiacheng Wu <>
2018-11-13 22:28:07 +0800

    fix bug and update document

--- src/test/java/seedu/scheduler/storage/XmlSerializableSchedulerTest.java
+++ src/test/java/seedu/scheduler/storage/XmlSerializableSchedulerTest.java
@@ -1,5 +1,7 @@
 package seedu.scheduler.storage;
 
+import static org.junit.Assert.assertEquals;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -9,6 +11,8 @@ import org.junit.rules.ExpectedException;
 
 import seedu.scheduler.commons.exceptions.IllegalValueException;
 import seedu.scheduler.commons.util.XmlUtil;
+import seedu.scheduler.model.Scheduler;
+import seedu.scheduler.testutil.TypicalEvents;
 
 public class XmlSerializableSchedulerTest {
 
@@ -18,6 +22,16 @@ public class XmlSerializableSchedulerTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void toModelType_typicalEventsFile_success() throws Exception {
+        XmlSerializableScheduler dataFromFile = XmlUtil.getDataFromFile(TYPICAL_EVENTS_FILE,
+                XmlSerializableScheduler.class);
+        Scheduler schedulerFromFile = dataFromFile.toModelType();
+        Scheduler typicalEventsScheduler = TypicalEvents.getTypicalScheduler();
+        assertEquals(schedulerFromFile, typicalEventsScheduler);
+    }
+
 
     @Test
     public void toModelType_invalidEventFile_throwsIllegalValueException() throws Exception {
